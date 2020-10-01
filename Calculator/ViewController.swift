f4c2dd809a15c156d654bf69fee097c548156f3d
Dhrumil <dhrumilmalaviya@hotmail.com>
2020-10-01 17:07:14 -0400

    Finalized the UI with some minor changes to make UI more attractive

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -1,20 +1,21 @@
 //
 //  ViewController.swift
-//  Calculator
-//
-//  Created by Dhrumil Malaviya on 2020-09-29
-//
+//  File Name: Calculator
+//  Author's name: Created by Dhrumil Malaviya on 2020-09-29
+//  StudentID :301058391
+//  App description: A simple mobile calculator UI is built in this version. Additional features will be added in upcoming versions
+//  Version 1.0
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController
+{
 
     // MARK: - UIElements
     @IBOutlet weak var lblResult: UILabel!
     @IBOutlet weak var lblCalculation: UILabel!
     @IBOutlet weak var btn1: UIButton!
     @IBOutlet weak var btn2: UIButton!
-    
     @IBOutlet weak var btn3: UIButton!
     @IBOutlet weak var btn4: UIButton!
     @IBOutlet weak var btn5: UIButton!
@@ -35,32 +36,21 @@ class ViewController: UIViewController {
     @IBOutlet weak var btn20: UIButton!
     @IBOutlet weak var stackView: UIStackView!
     @IBOutlet var allDigitsCollection: [UIButton]!
-    @IBOutlet weak var stackView1: UIStackView!
     @IBOutlet var viewShadow: [UIView]!
     
-  
-    
-    // MARK: - Variables
-
-    //imageView.transform=CGAffin
-    
-    // MARK: - ViewController methods
+    // MARK: - ViewController Methods
         
-    override func viewDidLoad() {
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
-        
         initViews()
-        self.view.backgroundColor=UIColor (red: 55/255, green: 67/255, blue: 83/255, alpha: 1)
-       // stackView1.backgroundColor=UIColor (red: 55/255, green: 67/255, blue: 83/255, alpha: 1)
-        
+        self.view.backgroundColor=UIColor (red: 55/255, green: 67/255, blue: 83/255, alpha: 1)      //setting the background color of the safearea
     }
-
-    // MARK: - Click events
     
     // MARK: - Methods
     func initViews()
     {
-        for variable in allDigitsCollection
+        for variable in allDigitsCollection                                                        //using for loops to apply designs to all buttons
         {
             buttonStyle(button: variable)
         }
@@ -72,24 +62,23 @@ class ViewController: UIViewController {
     
     func buttonStyle(button :UIButton)
     {
-        //button.layer.shadowColor = UIColor(red: 67/255, green: 79/255, blue: 93/255, alpha: 1).cgColor
         
-        button.layer.shadowRadius = 5
-        //button.layer.shadowOpacity = 1
         button.titleLabel?.font = .systemFont(ofSize: 30)
         button.backgroundColor = UIColor(red: 55/255, green: 67/255, blue: 83/255, alpha: 1)
         button.layer.shadowColor = UIColor.white.cgColor
-        button.layer.shadowOffset=CGSize(width: -3, height: -3)
-        button.layer.shadowOpacity = 0.1
+        button.layer.shadowOffset=CGSize(width: -3, height: -3)                                     //setting left and top shadow to desired color
+        button.layer.shadowRadius = 5
+        button.layer.shadowOpacity = 0.2
         button.layer.cornerRadius=15
     }
     
     func viewStyle(view :UIView)
     {
         view.layer.shadowColor = UIColor.black.cgColor
-        view.layer.shadowOffset=CGSize(width: 3, height: 3)
+       view.backgroundColor = UIColor(red: 55/255, green: 67/255, blue: 83/255, alpha: 1)
+        view.layer.shadowOffset=CGSize(width: 3, height: 3)                                        //setting right and bottom shadow desired color
         view.layer.shadowRadius = 5
-        view.layer.shadowOpacity = 0.5
+        view.layer.shadowOpacity = 0.4
         view.layer.cornerRadius=15
     }
 
